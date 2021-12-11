537b81d608638e143fa41f96c75eb669ac2b2cfb
Jerry Ng <ngshiheng@gmail.com>
2021-12-12 00:49:52 +0800

    add mvp version of the worker

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-const LEETCODE_API_ENDPOINT = 'https://leetcode.com/graphql'
-const TODOIST_API_ENDPOINT = 'https://api.todoist.com/rest/v1'
+import {
+    DAILY_CODING_CHALLENGE_QUERY,
+    LEETCODE_API_ENDPOINT,
+    TODOIST_API_ENDPOINT,
+} from './const'
+
+/**
+ * Sync LeetCode daily coding challenge to Todoist
+ */
+const syncLeetCodeCodingChallenge = async event => {
+    const question = await fetchDailyCodingChallenge()
+    createTodoistTask(question)
+}
+
+const fetchDailyCodingChallenge = async () => {
+    const init = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query: DAILY_CODING_CHALLENGE_QUERY }),
+    }
+
+    const response = await fetch(LEETCODE_API_ENDPOINT, init)
+    return response.json()
+}
+
+const createTodoistTask = async question => {
+    const questionTitle = question.data.activeDailyCodingChallengeQuestion.title
+    const questionLink = `https://leetcode.com${question.data.activeDailyCodingChallengeQuestion.link}`
+
+    const init = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${TODOIST_API_ENDPOINT}`,
+        },
+        body: {
+            content: `[${questionTitle}](${questionLink})`,
+            due_string: 'Today',
+        },
+    }
+
+    const response = await fetch(LEETCODE_API_ENDPOINT, init)
+    return response.json()
+}
 
 addEventListener('scheduled', event => {
     event.waitUntil(syncLeetCodeCodingChallenge(event))
 })
-
-const fetchDailyCodingChallenge = async () => {}
-
-const syncLeetCodeCodingChallenge = async event => {}
-
-const createTodoistTask = async () => {}
